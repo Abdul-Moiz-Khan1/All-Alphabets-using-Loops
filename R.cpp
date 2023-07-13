7f6ee333a6d292bcdcfc70feef239c857451fabb
Abdul-Moiz-Khan1 <136478539+Abdul-Moiz-Khan1@users.noreply.github.com>
2023-07-13 21:48:11 +0500

    Add files via upload
    C++ Programs that Prints Characters using sterics

---
+++ R.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i,j;
+           for(i=1;i<=5;i++)
+                            {
+                             for(j=1;j<=5;j++)
+                                              {
+                                               if(j==1 || i==1 || (j==5 && i<=3) || i==3 || (i==j && i>=3))
+                                                                         {
+                                                                          cout<<"*";     
+                                                                         }
+                                                                          else
+                                                                              {
+                                                                               cout<<" ";           
+                                                                              }
+                                              }    
+                             cout<<endl;
+                            }
+   
+    system("PAUSE");
+    return 0;
+}
---
+++ U.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int i,j;
+           for(i=1;i<=5;i++)
+                            {
+                             for(j=1;j<=5;j++)
+                                              {
+                                               if(j==1 || i==5 || j==5)
+                                                                         {
+                                                                          cout<<"*";     
+                                                                         }
+                                                                          else
+                                                                              {
+                                                                               cout<<" ";           
+                                                                              }
+                                              }    
+                             cout<<endl;
+                            }
+   
+    system("PAUSE");
+    return 0;
+}
